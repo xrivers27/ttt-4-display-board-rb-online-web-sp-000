4191157936cd6a87e9829a0c164ce2ed3c45540d
Lorin Xavier Rivers <xrivers27@gmail.com>
2021-01-26 17:42:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts "#{ }", "#{ }", "#{ }", "#{ }", "#{ }", "#{ }", "#{ }", "#{ }", "#{ }"
 end
 
-display_board(board[])
+display_board(board)
 
 
 #def display_board(board)
